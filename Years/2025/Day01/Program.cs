491743d95f62a86a71bdbcebc28b41dad89df7d8
xXrederXx <morgenthalerthierry@gmail.com>
2026-04-06 19:13:16 +0200

    feat: finish 2025 '1

---
+++ Years/2025/Day01/Program.cs
@@ -0,0 +1,73 @@
+using AoC.Common;
+
+namespace AoC.Y2025.Day01;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Advent of Code 2025 - Day 01");
+
+        string[] input = FileHelper.GetLines("data/input.txt");
+
+        System.Console.WriteLine("Part 1: " + Part1(input));
+        System.Console.WriteLine("Part 2: " + Part2(input));
+    }
+
+    static string Part1(string[] input)
+    {
+        int[] values = input
+            .Select(x => (x[0], int.Parse(x.Substring(1))))
+            .Select(x => x.Item1 == 'L' ? -x.Item2 : x.Item2)
+            .ToArray();
+        int timesAtZero = 0;
+        int current = 50;
+
+        foreach (int tick in values)
+        {
+            current += tick;
+            current %= 100;
+
+            if (current == 0)
+            {
+                timesAtZero++;
+            }
+        }
+
+        return timesAtZero.ToString();
+    }
+
+    static string Part2(string[] input)
+    {
+        int[] values = input
+            .Select(x => (x[0], int.Parse(x.Substring(1))))
+            .Select(x => x.Item1 == 'L' ? -x.Item2 : x.Item2)
+            .ToArray();
+        int timesAtZero = 0;
+        int current = 50;
+
+        foreach (int tick in values)
+        {
+            System.Console.Write($"Current: {current}\tTick: {tick}");
+            timesAtZero += Math.Abs(tick / 100);
+            int start = current;
+            current = (current + (tick % 100) + 100) % 100;
+
+            if (tick > 0 && start > current && start != 0)
+            {
+                timesAtZero++;
+            }
+            else if (tick < 0 && start < current && start != 0)
+            {
+                timesAtZero++;
+            }
+            else if (current == 0)
+            {
+                timesAtZero++;
+            }
+            System.Console.WriteLine($"\tTimes @ 0: {timesAtZero}");
+        }
+
+        return timesAtZero.ToString();
+    }
+}
